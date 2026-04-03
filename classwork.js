1c1a743a3df344f9633d1b70f3f9f155a453e1e2
soruto752 <sekoniireoluwa@gmail.com>
2026-04-03 19:34:52 +0100

    wrong

---
+++ classwork.js
@@ -0,0 +1,29 @@
+function exercise1() {
+  let amaraAgeBe = 17;
+  let tundeAgeBe = 12;
+
+  let diffferenceInAge = amaraAgeBe - tundeAgeBe;
+  let totalAge = amaraAgeBe + tundeAgeBe;
+
+  let report = "<div class='budget-card'>";
+  report += `<p> difference in age:<b> ${diffferenceInAge} </b></p>`;
+  report += `<p> totalAge:<b> ${totalAge} </b></p>`;
+  report += `</div>`;
+  return plus;
+}
+let value = exercise1();
+console.log(value);
+
+function exercise2() {
+  let amountoFNotebookBought = 3;
+  let amountoFPenBought = 2;
+  let costoFNotebook = 450;
+  let costoFPens = 120;
+
+  let totalCostoFPen = costoFPens * amountoFPenBought;
+  let totalCostoFNotebook = costoFNotebook * amountoFNotebookBought;
+  let totalCost = totalCostoFPen + totalCostoFNotebook;
+  return plus;
+}
+let worth = exercise2();
+console.log(worth);
