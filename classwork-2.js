e925525debd3c47743762f70f01ef501f326164a
soruto752 <sekoniireoluwa@gmail.com>
2026-04-10 09:51:15 +0100

    incmplete

---
+++ classwork-2.js
@@ -0,0 +1,15 @@
+function exercise() {
+  let monthElectricity = 185;
+  let first100UnitsCost = 12; //as in naira
+  let unitsMoreThan100 = 18; //as in naira
+
+  let firstUnits = first100UnitsCost * 100;
+  let moreThanUnits = unitsMoreThan100 * 85;
+  let totalElectricitySPENT = firstUnits + moreThanUnits;
+
+  if (higher >= 12500) {
+    myComment = "too high ";
+  } else if (lower <= 12500) {
+    myComment = "normal!";
+  }
+}
